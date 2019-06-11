97a95a0955a2e8d10bf99c960d695085de7a8abe
Y08L <Y08L@nykredit.dk>
2019-06-11 22:24:37 +0200

    Sprint 7 refactor of displaying all menu from komputronik

--- src/main/java/com/zygstore/utils/ReadCSVFileWithAllCategories.java
+++ src/main/java/com/zygstore/utils/ReadCSVFileWithAllCategories.java
@@ -1,18 +1,13 @@
 package com.zygstore.utils;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 
 import org.springframework.core.io.ClassPathResource;
-import sun.misc.ClassLoaderUtil;
 
 public class ReadCSVFileWithAllCategories {
     private BufferedReader reader;
@@ -21,17 +16,12 @@ public class ReadCSVFileWithAllCategories {
 
     public ReadCSVFileWithAllCategories(String filePath) {
         this.filePath = filePath;
-
     }
 
     private void readLines(String filePath) {
         try {
-
             BufferedReader reader = new BufferedReader(
                 new InputStreamReader(new ClassPathResource(filePath).getInputStream()));
-
-
-//            reader = new BufferedReader(new FileReader(filePath));
             String line = reader.readLine();
             while (line != null) {
                 if ((!line.equals("ID;ParentID;Name")) && (!line.equals("")) && (!line.equals(" "))) {
--- src/main/java/com/zygstore/utils/ReadKomputronikSite.java
+++ src/main/java/com/zygstore/utils/ReadKomputronikSite.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 
 /**
  * Place description here.
@@ -22,7 +23,6 @@ public class ReadKomputronikSite {
     private String title;
     private Element tree;
 
-
     public ReadKomputronikSite() throws IOException {
         this.doc = Jsoup.connect(WEB_STIE).get();
         this.title = doc.title();
@@ -33,61 +33,42 @@ public class ReadKomputronikSite {
     private ArrayList<String> getCSVLinesFromKomputronik(Element tree) {
         String id;
         String parentID;
-
-
         IdGenerator idGenerator = new IdGenerator();
 
         for (int i = 0; i < tree.childNodeSize(); i++) {
-            String html = tree.childNode(i).toString();
-            Document doc = Jsoup.parse(html);
-            Element link = doc.select("a").first();
+            id = Long.toString(idGenerator.generateNewId());
+            parentID = "0";
 
-            if (link != null) {
-                id = Long.toString(idGenerator.generateNewId());
-
-                parentID = "0";
-
-                String title = link.attr("title").toString();
-                String ulr = link.attr("href").toString();
-                linesFromFile.add(id + ";" + parentID + ";" + title);
-//feeding next menu level
+            if (addElementToList(tree.childNode(i), parentID)) {
                 String parentID2 = id;
                 for (int j = 0; j < tree.childNode(i).childNode(3).childNodeSize(); j++) {
-                    String html2 = tree.childNode(i).childNode(3).childNode(j).toString();
-                    Document doc2 = Jsoup.parse(html2);
-                    Element link2 = doc2.select("a").first();
-
-                    if (link2 != null) {
-                        id = Long.toString(idGenerator.generateNewId());
-                        String titleSecondLink = link2.attr("title").toString();
-                        String urlSecondLink = link2.attr("href").toString();
-                        linesFromFile.add(id + ";" + parentID2 + ";" + titleSecondLink);
-
-//feeding next menu level
+                    if (addElementToList(tree.childNode(i).childNode(3).childNode(j), parentID2)) {
                         String parentID3 = id;
                         for (int x = 0; x < tree.childNode(i).childNode(3).childNode(j).childNode(3).childNodes().size(); x++) {
-                            String html3 = tree.childNode(i).childNode(3).childNode(j).childNode(3).childNode(x).toString();
-                            Document doc3 = Jsoup.parse(html3);
-                            Element link3 = doc3.select("a").first();
-
-                            if (link3 != null) {
-                                id = Long.toString(idGenerator.generateNewId());
-                                String titleThirdLink = link3.attr("title").toString();
-                                String urlThirdLink = link3.attr("href").toString();
-                                linesFromFile.add(id + ";" + parentID3 + ";" + titleThirdLink);
-                            }
-
+                            addElementToList(tree.childNode(i).childNode(3).childNode(j).childNode(3).childNode(x), parentID3);
                         }
-
                     }
                 }
-
             } //link
-
         }
 
-
         return linesFromFile;
+    }
+
+    private Boolean addElementToList(Node tree, String parentID3) {
+        IdGenerator idGenerator = new IdGenerator();
+        String html3 = tree.toString();
+        Document doc3 = Jsoup.parse(html3);
+        Element link3 = doc3.select("a").first();
+
+        if (link3 != null) {
+            String id = Long.toString(idGenerator.generateNewId());
+            String titleThirdLink = link3.attr("title").toString();
+            String urlThirdLink = link3.attr("href").toString();
+            linesFromFile.add(id + ";" + parentID3 + ";" + titleThirdLink);
+            return true;
+        }
+        return false;
     }
 
     public Document getDoc() {
