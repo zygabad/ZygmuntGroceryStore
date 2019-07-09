3f5333026ce54ee472a4d427b6997ce36bd680cb
Y08L <Y08L@nykredit.dk>
2019-07-09 11:05:08 +0200

    Sprint 11 pusta lista produktow, produkty wyswietlane pod wzgledem kategorii, gwiazdki zloty kolor, wiekszy panel, skalowane obrazki

--- src/main/java/com/zygstore/service/ProductService.java
+++ src/main/java/com/zygstore/service/ProductService.java
@@ -13,7 +13,7 @@ import com.zygstore.dto.ProductDTO;
  */
 
 public class ProductService {
-    public List<ProductDTO> createProductsDTO() {
+    private List<ProductDTO> createProductsDTO() {
         List<ProductDTO> listOfProducts = new ArrayList<>();
 
         List<String> description1 = new ArrayList<String>();
@@ -21,7 +21,7 @@ public class ProductService {
         description1.add("typ procesora : Intel Core i5");
         description1.add("wielkość pamięci RAM : 8 GB");
         description1.add("pojemność dysku SSD : 128 GB");
-        ProductDTO productDTO1 = new ProductDTO("Apple Macbook Air 13,3","1",4.58,4169, description1,"applemacbookair13.jpg");
+        ProductDTO productDTO1 = new ProductDTO("Apple Macbook Air 13,3","4",4.58,4169, description1,"applemacbookair13.jpg");
         listOfProducts.add(productDTO1);
 
         List<String> description2 = new ArrayList<String>();
@@ -29,7 +29,7 @@ public class ProductService {
         description2.add("typ procesora : Intel Core i7, Intel Core i5");
         description2.add("wielkość pamięci RAM : 8 GB, 16 GB");
         description2.add("pojemność dysku SSD : 128 GB");
-        ProductDTO productDTO2 = new ProductDTO("Apple Macbook Pro 15","1",5,5599, description2,"applemacbookpro15.jpg");
+        ProductDTO productDTO2 = new ProductDTO("Apple Macbook Pro 15","4",5,5599, description2,"applemacbookpro15.jpg");
         listOfProducts.add(productDTO2);
 
         List<String> description3 = new ArrayList<String>();
@@ -40,7 +40,7 @@ public class ProductService {
         description3.add("waga : 425 g");
         description3.add("szerokość : 98 mm");
         description3.add("wysokość : 35 mm");
-        ProductDTO productDTO3 = new ProductDTO("Apple TV","1",4.75,799, description3,"appletv.jpg");
+        ProductDTO productDTO3 = new ProductDTO("Apple TV","84",4.75,799, description3,"appletv.jpg");
         listOfProducts.add(productDTO3);
 
         List<String> description4 = new ArrayList<String>();
@@ -51,17 +51,37 @@ public class ProductService {
         description4.add("aparat fotograficzny z tyłu : 12 Mpix");
         description4.add("pamięć RAM : 2048 MB");
         description4.add("kolor : czarny, różowy, srebrny, złoty");
-        ProductDTO productDTO4 = new ProductDTO("Apple Iphone 7","1",3.75,1979, description4,"appleIphone7.jpg");
+        ProductDTO productDTO4 = new ProductDTO("Apple Iphone 7","187",3.75,1979, description4,"appleIphone7.jpg");
         listOfProducts.add(productDTO4);
 
         List<String> description5 = new ArrayList<String>();
         description5.add("model procesora : Apple A12X");
         description5.add("wbudowany odbiornik GPS : tak");
         description5.add("aparat fotograficzny z tyłu : 12 Mpix");
-        ProductDTO productDTO5 = new ProductDTO("Apple Ipad pro 11","1",4.65,3799, description5,"appleipadpro.jpg");
+        ProductDTO productDTO5 = new ProductDTO("Apple Ipad pro 11","7",4.65,3799, description5,"appleipadpro.jpg");
         listOfProducts.add(productDTO5);
 
 
         return listOfProducts;
     }
-}
+
+    public List<ProductDTO> productListWithCategoryToDisplay (String category) {
+        List<ProductDTO> listOfAllProducts = createProductsDTO();
+        List<ProductDTO> listOfProductsInCategory = createProductsDTOByCategory(category, listOfAllProducts);
+        return listOfProductsInCategory;
+    }
+
+    private List<ProductDTO> createProductsDTOByCategory (String catogorytToDisplay, List<ProductDTO> listOfProducts) {
+        List<ProductDTO> listOfProductsInCategory = new ArrayList<>();
+
+        for (int i = 0; i < listOfProducts.size(); i++) {
+            if (listOfProducts.get(i).getCategoryId().equals(catogorytToDisplay)) {
+                listOfProductsInCategory.add(listOfProducts.get(i));
+            }
+        }
+
+        return listOfProductsInCategory;
+    }
+
+
+    }
